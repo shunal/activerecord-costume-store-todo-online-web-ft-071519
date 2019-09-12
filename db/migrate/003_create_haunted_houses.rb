06e840fd2a2915b39aab4ef7e119f6a3755279b2
Steven Hunal <stevehunal@gmail.com>
2019-09-12 04:37:46 +0000

    Automatically backed up by Learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -1,1 +1,14 @@
 # Create your haunted_houses migration here
+class CreateHauntedHouses < ActiveRecord::Migration[5.2]
+  
+  create_table :haunted_houses do |column|
+    column.string :name
+    column.string :location
+    column.string :theme
+    column.integer :price
+    column.boolean :family_friendly
+    column.datetime :opening_date
+    column.datetime :closing_date
+    column.text :description
+  end 
+end 
